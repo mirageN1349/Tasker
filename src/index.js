6086602ff961f8993238094d0acb33b30bf211fb
mirageN1349 <desertirpipi@gmail.com>
2020-05-19 20:13:14 +0300

    css-layout finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import { Tasker } from './components/tasker/Tasker'
+import { Header } from './components/header/Header'
+import { Toolbar } from './components/toolbar/Toolbar'
+import { Draglist } from './components/draglist/Draglist'
 import './sass/index.sass'
